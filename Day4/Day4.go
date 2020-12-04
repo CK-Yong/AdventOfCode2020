1cee55b902335ccece261df6618463d14f121cde
Chee Kent Yong <chee.kent.yong@rtl.nl>
2020-12-04 22:06:48 +0100

    Day 4 (wip, part 2 incorrect)

---
+++ Day4/Day4.go
@@ -0,0 +1,173 @@
+package main
+
+import (
+    "fmt"
+    "strconv"
+    "strings"
+)
+
+type Passport struct {
+    input    string
+    innerMap map[string]string
+}
+
+func toKeyValuePair(value string) (string, string) {
+    array := strings.Split(value, ":")
+    return array[0], array[1]
+}
+
+func (p *Passport) Init(input string) *Passport {
+    p.innerMap = make(map[string]string)
+    sanitized := strings.Replace(input, "\n", " ", -1)
+    array := strings.Split(sanitized, " ")
+    for _, line := range array {
+        key, val := toKeyValuePair(line)
+        p.innerMap[key] = val
+    }
+    return p
+}
+
+func (p Passport) IsValid() bool {
+    _, cidExists := p.innerMap["cid"]
+    if cidExists {
+        return len(p.innerMap) == 8
+    } else {
+        return len(p.innerMap) == 7
+    }
+}
+
+/* Logic for Part 2 */
+
+func (p Passport) IsValidV2() bool {
+    if !p.IsValid() {
+        return false
+    }
+
+    data := p.innerMap
+
+    birthYear, _ := strconv.Atoi(data["byr"])
+    if birthYear < 1920 || birthYear > 2002 {
+        return false
+    }
+
+    issueYear, _ := strconv.Atoi(data["iyr"])
+    if issueYear < 2010 || issueYear > 2020 {
+        return false
+    }
+
+    expirationYear, _ := strconv.Atoi(data["eyr"])
+    if expirationYear < 2020 || expirationYear > 2030 {
+        return false
+    }
+
+    heightIsValid := p.hasValidHeight()
+    if !heightIsValid {
+        return false
+    }
+
+    hairColorIsValid := p.hasValidHairColor()
+    if !hairColorIsValid {
+        return false
+    }
+
+    eyeColorIsValid := p.hasValidEyeColor()
+    if !eyeColorIsValid {
+        return false
+    }
+
+    passportIdIsValid := p.hasValidPassportId()
+    if !passportIdIsValid {
+        return false
+    }
+
+    return true
+}
+
+func (p Passport) hasValidHeight() bool {
+    height := p.innerMap["hgt"]
+
+    if strings.Contains(height, "in") {
+        val := strings.Replace(height, "in", "", -1)
+        intVal, _ := strconv.Atoi(val)
+        if intVal < 59 || intVal > 76 {
+            return false
+        }
+    }
+
+    if strings.Contains(height, "cm") {
+        val := strings.Replace(height, "cm", "", -1)
+        intVal, _ := strconv.Atoi(val)
+        if intVal < 150 || intVal > 193 {
+            return false
+        }
+    }
+
+    return true
+}
+
+func (p *Passport) hasValidHairColor() bool {
+    hcl := p.innerMap["hcl"]
+
+    if !strings.HasPrefix(hcl, "#") || len(hcl) != 7 {
+        return false
+    }
+
+    alphaNumerics := "0123456789abcdef"
+    for i := 1; i < len(hcl); i++ {
+        if !strings.ContainsRune(alphaNumerics, rune(hcl[i])) {
+            return false
+        }
+    }
+
+    return true
+}
+
+func (p *Passport) hasValidEyeColor() bool {
+    validEyeColors := strings.Split("amb blu brn gry grn hzl oth", " ")
+    eyeColor := p.innerMap["ecl"]
+    for _, validColor := range validEyeColors {
+        if eyeColor == validColor {
+            return true
+        }
+    }
+    return false
+}
+
+func (p *Passport) hasValidPassportId() bool {
+    passportId := p.innerMap["pid"]
+    if len(passportId) != 9 {
+        return false
+    }
+
+    for _, character := range passportId {
+        if character < '0' || character > '9' {
+            return false
+        }
+    }
+    return true
+}
+
+func main() {
+    inputArray := strings.Split(PuzzleInput, "\n\n")
+    var count int
+    for _, passportInput := range inputArray {
+        passport := new(Passport)
+        passport.Init(passportInput)
+        if passport.IsValid() {
+            count++
+        }
+    }
+
+    fmt.Printf("Part 1: %v valid passports\n", count)
+
+    count = 0
+    for _, passportInput := range inputArray {
+        passport := new(Passport)
+        passport.Init(passportInput)
+        if passport.IsValidV2() {
+            count++
+        }
+    }
+
+    fmt.Printf("Part 2: %v valid passports", count)
+}
---
+++ Day4/Day4_test.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+    "testing"
+)
+
+var validPassports = []string{
+    "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\nbyr:1937 iyr:2017 cid:147 hgt:183cm",
+    "hcl:#ae17e1 iyr:2013\neyr:2024\necl:brn pid:760753108 byr:1931\nhgt:179cm",
+}
+
+func Test_should_be_valid_passport(test *testing.T) {
+    for _, input := range validPassports {
+        passport := new(Passport)
+        passport.Init(input)
+
+        result := passport.IsValid()
+        if result == false {
+            test.Errorf("Expected passport %v to be valid. Got %v", input, result)
+        }
+    }
+}
+
+var invalidPassports = []string{
+    "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\nhcl:#cfa07d byr:1929",
+    "hcl:#cfa07d eyr:2025 pid:166559648\niyr:2011 ecl:brn hgt:59in",
+}
+
+func Test_should_be_invalid_passport(test *testing.T) {
+    for _, input := range invalidPassports {
+        passport := new(Passport)
+        passport.Init(input)
+
+        result := passport.IsValid()
+        if result == true {
+            test.Errorf("Expected passport %v to be invalid. Got %v", input, result)
+        }
+    }
+}
+
+var validPassportsV2 = []string{
+    "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980\nhcl:#623a2f",
+    "eyr:2029 ecl:blu cid:129 byr:1989\niyr:2014 pid:896056539 hcl:#a97842 hgt:165cm",
+    "hcl:#888785\nhgt:164cm byr:2001 iyr:2015 cid:88\npid:545766238 ecl:hzl\neyr:2022",
+    "iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719",
+}
+
+func Test_should_be_valid_V2_passports(test *testing.T){
+    for _, input := range validPassportsV2 {
+        passport := new(Passport)
+        passport.Init(input)
+
+        result := passport.IsValidV2()
+        if result == false {
+            test.Errorf("Expected passport %v to be valid. Got %v", input, result)
+        }
+    }
+}
+
+var invalidPassportsV2 = []string{
+    "eyr:1972 cid:100\nhcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926",
+    "iyr:2019\nhcl:#60297 eyr:1967 hgt:170cm\necl:grn pid:012533040 byr:1946",
+    "hcl:dab227 iyr:2012\necl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277",
+    "hgt:59cm ecl:zzz\neyr:2038 hcl:74454a iyr:2023\npid:3556412378 byr:2007",
+}
+
+func Test_should_be_invalid_V2_passports(test *testing.T){
+    for _, input := range invalidPassportsV2 {
+        passport := new(Passport)
+        passport.Init(input)
+
+        result := passport.IsValidV2()
+        if result == true {
+            test.Errorf("Expected passport %v to be invalid. Got %v", input, result)
+        }
+    }
+}
+
+func Test_should_be_invalid_hair_color(test *testing.T){
+    passport := new(Passport)
+    passport.Init("hcl:#123abz")
+
+    result := passport.hasValidHairColor()
+
+    if result == true {
+        test.Errorf("Expected invalid hair color")
+    }
+}
