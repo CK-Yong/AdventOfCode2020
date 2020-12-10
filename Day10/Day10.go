d56ff6e68e68c1eaaaf8377321bdc13f07e1f97c
Chee Kent Yong <cheekentyong@gmail.com>
2020-12-11 00:43:40 +0100

    Finished Day10 Part 2

--- Day10/Day10.go
+++ Day10/Day10.go
@@ -1,58 +1,133 @@
 package main
 
 import (
-	"fmt"
-	"strconv"
-	"strings"
+    "fmt"
+    "sort"
+    "strconv"
+    "strings"
 )
 
 func main() {
-	parsed := strings.Split(PuzzleInput, "\n")
+    parsed := strings.Split(PuzzleInput, "\n")
 
-	adapterSet := AdapterSet{map[int]int{1: 0, 2: 0, 3: 0}}
-	adapterSet.Traverse(parsed)
+    adapterSet := AdapterSet{map[int]int{1: 0, 2: 0, 3: 0}}
+    adapterSet.Traverse(parsed)
 
-	fmt.Printf("Part 1: 1 jolt * 3 jolt counts: %v", adapterSet.Values[1]*adapterSet.Values[3])
+    fmt.Printf("Part 1: 1 jolt * 3 jolt counts: %v\n", adapterSet.Values[1]*adapterSet.Values[3])
+
+    arrangementCount := adapterSet.CountPossibleArrangements(parsed)
+    fmt.Printf("Part 2: total number of arrangements: %f", arrangementCount)
 }
 
 type AdapterSet struct {
-	Values map[int]int
+    Values map[int]int
 }
 
 func (set AdapterSet) Traverse(input []string) {
-	currentValue := 0
-	for {
-		nextStep := FindNextStep(input, currentValue)
-		if nextStep < 0 {
-			set.Values[3]++ // Add own device
-			return
-		}
+    currentValue := 0
+    for {
+        nextStep := FindNextStep(input, currentValue)
+        if nextStep < 0 {
+            set.Values[3]++ // Add own device
+            return
+        }
 
-		set.Values[nextStep - currentValue]++
-		currentValue = nextStep
-	}
+        set.Values[nextStep-currentValue]++
+        currentValue = nextStep
+    }
 }
 
 func FindNextStep(input []string, currentValue int) int {
-	for i := 1; i < 4; i++ {
-		target := currentValue + i
-		if Contains(input, target) {
-			return target
-		}
-	}
-	return -1
+    for i := 1; i < 4; i++ {
+        target := currentValue + i
+        if Contains(input, target) {
+            return target
+        }
+    }
+    return -1
 }
 
 func Contains(input []string, target int) bool {
-	for _, val := range input {
-		number, _ := strconv.Atoi(val)
-		if target == number {
-			return true
-		}
-	}
-	return false
+    for _, val := range input {
+        number, _ := strconv.Atoi(val)
+        if target == number {
+            return true
+        }
+    }
+    return false
 }
 
-func (set AdapterSet) CountPossibleArrangements(parsed []string) int64 {
-	
+func (set AdapterSet) CountPossibleArrangements(input []string) float64 {
+    adapters := make([]int, 0)
+    for _, val := range input {
+        number, _ := strconv.Atoi(val)
+        adapters = append(adapters, number)
+    }
+
+    sort.Ints(adapters)
+    // Add initial value and my own device
+    adapters = append([]int{0}, adapters...)
+    adapters = append(adapters, adapters[len(adapters)-1]+3)
+
+    // We take slices of 3, so stop when there are two values left
+    omittable := make([]int, 0)
+    for i := 0; i < len(adapters)-2; i++ {
+        slice := adapters[i : i+3]
+        if slice[2]-slice[0] < 4 {
+            omittable = append(omittable, slice[1])
+        }
+    }
+
+    // Get all sequences
+    prev := omittable[0] - 1
+    omitSequences := make([][]int, 1)
+    currentRow := 0
+    for _, value := range omittable {
+        if value != prev+1 {
+            currentRow++
+            omitSequences = append(omitSequences, make([]int, 0))
+        }
+        omitSequences[currentRow] = append(omitSequences[currentRow], value)
+        prev = value
+    }
+
+    // Loop over sequences and search for valid permutations
+    count := float64(1)
+    for _, sequence := range omitSequences {
+        count *= CountPermutations(sequence)
+    }
+    return count
+}
+
+func CountPermutations(sequence []int) float64 {
+    // Because all these are omitabble, we know that start = [0] - 1, and end = [length - 1] + 1
+    sequence = append([]int{sequence[0] - 1}, sequence...)
+    sequence = append(sequence, sequence[len(sequence)-1]+1)
+
+    return GetPossibleBranches(sequence[0], sequence)
+}
+
+func GetPossibleBranches(value int, sequence []int) float64 {
+    last := sequence[len(sequence)-1]
+    if value == last {
+        return 1
+    }
+
+    count := float64(0)
+    for i := 1; i < 4; i++ {
+        next := value + i
+        if ContainsInt(sequence, next) {
+            count += GetPossibleBranches(next, sequence)
+        }
+    }
+    return count
+}
+
+func ContainsInt(sequence []int, value int) bool {
+    for _, val := range sequence {
+        if val == value {
+            return true
+        }
+    }
+    return false
 }
--- Day10/Day10_test.go
+++ Day10/Day10_test.go
@@ -32,3 +32,16 @@ func Test_should_find_all_possible_arrangements(test *testing.T) {
 		test.Errorf("Expected adapterset to find 8 arrangements. Got: %v", count)
 	}
 }
+
+
+func Test_should_find_all_possible_arrangements_2(test *testing.T) {
+	input := "28\n33\n18\n42\n31\n14\n46\n20\n48\n47\n24\n23\n49\n45\n19\n38\n39\n11\n1\n32\n25\n35\n8\n17\n7\n9\n4\n2\n34\n10\n3"
+	parsed := strings.Split(input, "\n")
+
+	adapterSet := AdapterSet{map[int]int{1: 0, 2: 0, 3: 0}}
+	count := adapterSet.CountPossibleArrangements(parsed)
+
+	if count != 19208 {
+		test.Errorf("Expected adapterset to find 19208 arrangements. Got: %v", count)
+	}
+}
