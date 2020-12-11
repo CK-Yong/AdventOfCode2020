54443c7c40c82e5eba65359bd826099358e0ee38
Chee Kent Yong <cheekentyong@gmail.com>
2020-12-11 22:48:02 +0100

    Added quick benchmark for Day 10

--- Day10/Day10_test.go
+++ Day10/Day10_test.go
@@ -6,7 +6,6 @@ import (
 	"testing"
 )
 
-
 func Test_should_traverse_all_adapters_and_give_back_difference(test *testing.T) {
 	input := "16\n10\n15\n5\n1\n11\n7\n19\n6\n12\n4"
 	parsed := strings.Split(input, "\n")
@@ -33,7 +32,6 @@ func Test_should_find_all_possible_arrangements(test *testing.T) {
 	}
 }
 
-
 func Test_should_find_all_possible_arrangements_2(test *testing.T) {
 	input := "28\n33\n18\n42\n31\n14\n46\n20\n48\n47\n24\n23\n49\n45\n19\n38\n39\n11\n1\n32\n25\n35\n8\n17\n7\n9\n4\n2\n34\n10\n3"
 	parsed := strings.Split(input, "\n")
@@ -43,5 +41,14 @@ func Test_should_find_all_possible_arrangements_2(test *testing.T) {
 
 	if count != 19208 {
 		test.Errorf("Expected adapterset to find 19208 arrangements. Got: %v", count)
+	}
+}
+
+func BenchmarkAdapterSet_CountPossibleArrangements(test *testing.B) {
+	parsed := strings.Split(PuzzleInput, "\n")
+	adapterSet := AdapterSet{map[int]int{1: 0, 2: 0, 3: 0}}
+
+	for n := 0; n < test.N; n++{
+		adapterSet.CountPossibleArrangements(parsed)
 	}
 }
