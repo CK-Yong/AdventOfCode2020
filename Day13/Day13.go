c400a7d73a4f8916f827f42da1e69fc901a138e4
Chee Kent Yong <cheekentyong@gmail.com>
2020-12-14 23:31:47 +0100

    Finished Day 13 Part 2

--- Day13/Day13.go
+++ Day13/Day13.go
@@ -13,7 +13,12 @@ func main() {
 	buses := CreateBuses(PuzzleInputTimestamp, service)
 	earliestBus := GetEarliestBus(buses)
 
-	fmt.Printf("Part 1: Earliest bus ID * number of minutes to wait: %v", earliestBus.Interval*(earliestBus.FirstDeparture-PuzzleInputTimestamp))
+	fmt.Printf("Part 1: Earliest bus ID * number of minutes to wait: %v\n", earliestBus.Interval*(earliestBus.FirstDeparture-PuzzleInputTimestamp))
+
+	buses = CreateBuses(-1, service)
+	earliestTimestamp := GetSyncedTimestamp(buses)
+
+	fmt.Printf("Part 2: Earliest timestamp: %v", earliestTimestamp)
 }
 
 type Bus struct {
@@ -73,4 +78,53 @@ func GetEarliestDeparture(timestamp int, interval int) int {
 			return currentDeparture
 		}
 	}
+}
+
+func GetSyncedTimestamp(buses []Bus) int64 {
+	ts := searchTimeStamp(0, 0, buses)
+	commonMultiplier := GetCommonMultiplier(buses)
+
+	for ts-int64(commonMultiplier) > 0 {
+		ts -= int64(commonMultiplier)
+	}
+	return ts
+}
+
+func GetCommonMultiplier(buses []Bus) int {
+	aggregator := 1
+	for _, bus := range buses {
+		if bus.Interval < 0 {
+			continue
+		}
+		aggregator *= bus.Interval
+	}
+	return aggregator
+}
+
+func searchTimeStamp(index int, timestamp int64, buses []Bus) int64 {
+	if index == len(buses) {
+		return 0
+	}
+
+	bus := buses[index]
+	interval := int64(bus.Interval)
+
+	if bus.Interval < 0 {
+		return searchTimeStamp(index+1, timestamp, buses)
+	}
+
+	newTimestamp := int64(1)
+	for i, bus := range buses {
+		if i == index || bus.Interval < 0 {
+			continue
+		}
+		newTimestamp *= int64(bus.Interval)
+	}
+
+	i := int64(0)
+	for ((newTimestamp*i)+int64(index))%interval != 0 {
+		i++
+	}
+
+	return (newTimestamp * i) + searchTimeStamp(index+1, newTimestamp, buses)
 }
