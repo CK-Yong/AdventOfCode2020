ba8bfd98cfadc5803685567b557875bcc6f4cc22
Chee Kent Yong <cheekentyong@gmail.com>
2020-12-16 19:50:38 +0100

    Dunno what I'm doing wrong

--- Day16/Day16.go
+++ Day16/Day16.go
@@ -1,109 +1,170 @@
 package main
 
 import (
-    "fmt"
-    "strconv"
-    "strings"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
-func main(){
-    tickets := ParseTickets(strings.Split(TicketInputs, "\n"))
-    rules := ParseRules(strings.Split(RuleInputs, "\n"))
+func main() {
+	tickets := ParseTickets(strings.Split(TicketInputs, "\n"))
+	rules := ParseRules(strings.Split(RuleInputs, "\n"))
 
-    result := ScanForErrors(tickets, rules)
-    fmt.Printf("Part 1: Ticket error rate is %v\n", result)
+	result := ScanForErrors(tickets, rules)
+	fmt.Printf("Part 1: Ticket error rate is %v\n", result)
 
-    validTickets := RemoveErrors(tickets, rules)
-    
+	numberOfIds := len(tickets[0].Numbers)
+	validTickets := RemoveErrors(tickets, rules)
+	labels := SortLabels(validTickets, rules, numberOfIds)
+	names := make([]string, numberOfIds)
+
+	for i, label := range labels {
+		names[i] = label.name
+	}
+
+	fmt.Printf("Part 2: Ticket order is %v", names)
+
 }
 
 type Ticket struct {
-    Numbers []int
-    isValid bool
+	Numbers []int
 }
 
 type Boundary struct {
-    Min, Max int
+	Min, Max int
 }
 
 type Rule struct {
-    name       string
-    boundaries []Boundary
+	name       string
+	boundaries []Boundary
 }
 
 func ScanForErrors(tickets []Ticket, rules []Rule) int {
-    errors := 0
-    for _, ticket := range tickets {
-        errors += ApplyRule(ticket, rules)
-    }
-    return errors
+	errors := 0
+	for _, ticket := range tickets {
+		errors += ApplyRule(ticket, rules)
+	}
+	return errors
 }
 
 func RemoveErrors(tickets []Ticket, rules []Rule) []Ticket {
-    validTickets := make([]Ticket, 0)
-    for _, ticket := range tickets {
-        errors := ApplyRule(ticket, rules)
-        if errors == 0 {
-            validTickets = append(validTickets, ticket)
-        }
-    }
-    return validTickets
+	validTickets := make([]Ticket, 0)
+	for _, ticket := range tickets {
+		isValid := isTicketValid(ticket, rules)
+		if isValid {
+			validTickets = append(validTickets, ticket)
+		}
+	}
+	return validTickets
+}
+
+func isTicketValid(ticket Ticket, rules []Rule) bool {
+	for _, num := range ticket.Numbers {
+		numIsValid := false
+		for _, rule := range rules {
+			if numIsValid {
+				break
+			}
+
+			if isValidNumber(num, rule) {
+				numIsValid = true
+				break
+			}
+		}
+		if !numIsValid {
+			return false
+		}
+	}
+	return true
 }
 
 func ApplyRule(ticket Ticket, rules []Rule) int {
-    errorRate := 0
+	errorRate := 0
 
-    for _, num := range ticket.Numbers {
-        numIsValid := false
-        for _, rule := range rules {
-            if numIsValid {
-                break
-            }
+	for _, num := range ticket.Numbers {
+		numIsValid := false
+		for _, rule := range rules {
+			if numIsValid {
+				break
+			}
 
-            for _, boundary := range rule.boundaries {
-                if boundary.Min <= num && boundary.Max >= num {
-                    numIsValid = true
-                    break
-                }
-            }
-        }
-        if !numIsValid {
-            errorRate += num
+			for _, boundary := range rule.boundaries {
+				if boundary.Min <= num && boundary.Max >= num {
+					numIsValid = true
+					break
+				}
+			}
+		}
+		if !numIsValid {
+			errorRate += num
+		}
+	}
+
+	return errorRate
+}
+
+func isValidNumber(number int, rule Rule) bool {
+    for _, boundary := range rule.boundaries {
+        if boundary.Min <= number && number <= boundary.Max {
+            return true
         }
     }
-
-    return errorRate
+    return false
 }
 
 func ParseRules(input []string) []Rule {
-    rules := make([]Rule, 0)
-    for _, line := range input {
-        nameValues := strings.Split(line, ": ")
-        boundaryInputs := strings.Split(nameValues[1], " or ")
-        boundaries := make([]Boundary, 2)
+	rules := make([]Rule, 0)
+	for _, line := range input {
+		nameValues := strings.Split(line, ": ")
+		boundaryInputs := strings.Split(nameValues[1], " or ")
+		boundaries := make([]Boundary, 2)
 
-        for i, boundaryStr := range boundaryInputs {
-            numStrs := strings.Split(boundaryStr, "-")
-            min, _ := strconv.Atoi(numStrs[0])
-            max, _ := strconv.Atoi(numStrs[1])
-            boundaries[i] = Boundary{min, max}
-        }
-        rules = append(rules, Rule{name: nameValues[0], boundaries: boundaries})
-    }
-    return rules
+		for i, boundaryStr := range boundaryInputs {
+			numStrs := strings.Split(boundaryStr, "-")
+			min, _ := strconv.Atoi(numStrs[0])
+			max, _ := strconv.Atoi(numStrs[1])
+			boundaries[i] = Boundary{min, max}
+		}
+		rules = append(rules, Rule{name: nameValues[0], boundaries: boundaries})
+	}
+	return rules
 }
 
 func ParseTickets(input []string) []Ticket {
-    tickets := make([]Ticket, 0)
+	tickets := make([]Ticket, 0)
 
-    for _, line := range input {
-        split := strings.Split(line, ",")
-        numbers := make([]int, 0)
-        for _, str := range split {
-            number, _ := strconv.Atoi(str)
-            numbers = append(numbers, number)
-        }
-        tickets = append(tickets, Ticket{Numbers: numbers})
-    }
-    return tickets
+	for _, line := range input {
+		split := strings.Split(line, ",")
+		numbers := make([]int, 0)
+		for _, str := range split {
+			number, _ := strconv.Atoi(str)
+			numbers = append(numbers, number)
+		}
+		tickets = append(tickets, Ticket{Numbers: numbers})
+	}
+	return tickets
+}
+
+func SortLabels(tickets []Ticket, rules []Rule, numbersPerTicket int) []Rule {
+	result := make([]Rule, len(rules))
+	for _, rule := range rules {
+		for j := 0; j < numbersPerTicket; j++ {
+			isValidRule := false
+			for i := range tickets {
+				number := tickets[i].Numbers[j]
+				if !isValidNumber(number, rule) {
+					isValidRule = false
+					break
+				} else {
+					isValidRule = true
+				}
+			}
+
+			if isValidRule {
+				result[j] = rule
+				break
+			}
+		}
+	}
+	return result
 }
--- Day16/Day16_test.go
+++ Day16/Day16_test.go
@@ -1,21 +1,41 @@
 package main
 
 import (
-    "strings"
-    "testing"
+	"reflect"
+	"strings"
+	"testing"
 )
 
 func Test_should_result_in_error_rate_of_71(test *testing.T) {
-    input := "7,3,47\n40,4,50\n55,2,20\n38,6,12"
-    tickets := ParseTickets(strings.Split(input, "\n"))
+	input := "7,3,47\n40,4,50\n55,2,20\n38,6,12"
+	tickets := ParseTickets(strings.Split(input, "\n"))
 
-    ruleInput := "class: 1-3 or 5-7\nrow: 6-11 or 33-44\nseat: 13-40 or 45-50"
-    rules := ParseRules(strings.Split(ruleInput, "\n"))
+	ruleInput := "class: 1-3 or 5-7\nrow: 6-11 or 33-44\nseat: 13-40 or 45-50"
+	rules := ParseRules(strings.Split(ruleInput, "\n"))
 
-    result := ScanForErrors(tickets, rules)
+	result := ScanForErrors(tickets, rules)
 
-    if result != 71 {
-        test.Errorf("Expected error rate to be 71. Got %v", result)
-    }
+	if result != 71 {
+		test.Errorf("Expected error rate to be 71. Got %v", result)
+	}
 }
 
+func Test_should_result_in_row_class_seat(test *testing.T) {
+	input := "11,12,13\n3,9,18\n15,1,5\n5,14,9"
+	tickets := ParseTickets(strings.Split(input, "\n"))
+
+	ruleInput := "class: 0-1 or 4-19\nrow: 0-5 or 8-19\nseat: 0-13 or 16-19"
+	rules := ParseRules(strings.Split(ruleInput, "\n"))
+
+	labels := SortLabels(tickets, rules, len(tickets[0].Numbers))
+
+	names := make([]string, 3)
+	for i, label := range labels {
+		names[i] = label.name
+	}
+
+	expected := []string{"row", "class", "seat"}
+	if !reflect.DeepEqual(names, expected) {
+		test.Errorf("Expected error rate to be %v. Got %v", expected, labels)
+	}
+}
